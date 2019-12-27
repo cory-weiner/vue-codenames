89847c7cc293377b87f7f4f8d94eb4bfd8325286
Cory Weiner <corymw@gmail.com>
2019-12-27 13:52:24 -0600

    with mqtt

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,39 @@
     This is the homepage
    <button v-on:click="generateBoard">New Board</button>
    <button v-on:click="showall=!showall">Toggle Answers</button>
+   <button v-on:click="pushboard">Push Board</button>
     <div class="tile_grid">
       <div class="tile" v-for="(tile,index) in gameboard.tiles" v-bind:key="index" :style={backgroundColor:getTileColor(tile)} v-on:click="guess(index)">{{tile.word}}</div>
     </div>
     {{scoreboard}}
+
+    <pre>{{client}}</pre>
   </div>
 </template>
 
 <script>
 import { mapFields } from 'vuex-map-fields';
 import { mapMutations, mapGetters } from 'vuex';
+var mqtt=require('mqtt');
 
 export default {
   name: 'home',
+  created(){
+    this.client = mqtt.connect(this.host+':'+this.port,{clientId:this.clientId})
+  },
   data(){
     return {
-      showall: false
+      showall: false,
+      client: {},
+      msglog: [],
+      host: 'wss://test.mosquitto.org',
+      port: '8081',
+      topic: 'codenames/',
+      lobby_id: 'qtz7',
+      subscribe_qos: 0,
+      publish_qos: 0,
+      retain:true,
+      clientId: 'client_' +  Date.now().toString(),
     }
   },
   computed: {
@@ -38,6 +55,9 @@ export default {
       else{
         return '#dfdfdf'
       }
+    },
+    pushboard(){
+      this.client.publish(this.topic+this.lobby_id,JSON.stringify(this.gameboard),{qos:parseInt(this.publish_qos),retain:this.retain})
     }
   }
 }
